e3b40eb0463b0fd214a5da490adadb23842e844d
rick-mingyu-liu <rickmyliu@gmail.com>
2024-10-16 17:17:35 +0800

    kitchen separable objects

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ClearCounter : MonoBehaviour {
 
+    [SerializeField] private KitchenObjectsSO kitchenObjectSO;
+    [SerializeField] private Transform counterTopPoint;
 
     public void Interact() {
         Debug.Log("Interact!");
+        Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
+        kitchenObjectTransform.localPosition = Vector3.zero;
+
     }
 
 
