3169310fa84008382f143417272ce8de4a8384f7
rick-mingyu-liu <rickmyliu@gmail.com>
2024-10-17 15:02:05 +0800

    interaction with the containerCounter implemented

--- Assets/Scripts/ContainerCounter.cs
+++ Assets/Scripts/ContainerCounter.cs
@@ -1,13 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class ContainerCounter : BaseCounter {
+    
+    public event EventHandler OnPlayerGrabbedObject;
 
     [SerializeField] private KitchenObjectsSO kitchenObjectSO;
 
     public override void Interact(Player player) {
         Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab);
         kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(player);
+
+        OnPlayerGrabbedObject?.Invoke(this, EventArgs.Empty);
     }
 }
