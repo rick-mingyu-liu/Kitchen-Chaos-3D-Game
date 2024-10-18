ac9e6dfc3c2338facee272ea52b8e99730b7e3f3
rick-mingyu-liu <rickmyliu@gmail.com>
2024-10-18 16:00:38 +0800

    plate load up visual update

--- Assets/Scripts/Counters/PlateCounterVisual.cs
+++ Assets/Scripts/Counters/PlateCounterVisual.cs
@@ -8,11 +8,22 @@ public class PlateCounterVisual : MonoBehaviour
     [SerializeField] private Transform counterTopPoint;
     [SerializeField] private Transform plateVisualPrefab;
 
+    private List<GameObject> plateVisualGameObjectList;
+
+    private void Awake() {
+        plateVisualGameObjectList = new List<GameObject>();
+    }
+
     private void Start() {
         platesCounter.OnPlateSpawned += PlatesCounter_OnPlateSpawned;
     }
 
     private void PlatesCounter_OnPlateSpawned(object render, System.EventArgs e) {
         Transform plateVisualTransform = Instantiate(plateVisualPrefab, counterTopPoint);
+
+        float plateOffsetY = .1f;
+        plateVisualTransform.localPosition = new Vector3(0, plateOffsetY * plateVisualGameObjectList.Count,0);
+
+        plateVisualGameObjectList.Add(plateVisualTransform.gameObject);
     }
 }
