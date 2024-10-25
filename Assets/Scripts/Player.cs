7add76fc89698c9f205169b99be95675e12b78ef
rick-mingyu-liu <rickmyliu@gmail.com>
2024-10-25 11:03:38 +0800

    game start implement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -41,12 +41,16 @@ public class Player : MonoBehaviour, IKitchenObjectParent {
     }
 
     private void GameInput_OnInteractAlternateAction(object sender, System.EventArgs f) {
+        if (!KitchenGameManager.Instance.IsGamePlaying()) return;
+
         if (selectedCounter != null) {
             selectedCounter.InteractAlternate(this);
         } 
     }
 
     private void GameInput_OnInteractAction(object sender, System.EventArgs e) {
+        if (!KitchenGameManager.Instance.IsGamePlaying()) return;
+        
         if (selectedCounter != null) {
             selectedCounter.Interact(this);
         }
---
+++ Assets/Scripts/UI/GameStartCountDownUI.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameStartCountDownUI : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI countdownText;
+
+
+    private void Start() {
+        KitchenGameManager.Instance.OnStateChanged += KitchenGameManager_OnStateChanged;
+
+        Hide();
+    }
+
+    private void KitchenGameManager_OnStateChanged(object sender, System.EventArgs e) {
+        if (KitchenGameManager.Instance.IsCountDownToStartActive()) {
+            Show();
+        } else {
+            Hide();
+        }
+    }
+
+    private void Update() {
+        countdownText.text  = Mathf.Ceil(KitchenGameManager.Instance.GetCountDownToStartTimer()).ToString();
+    }
+
+    private void Show() {
+        gameObject.SetActive(true);
+    }
+
+    private void Hide() {
+        gameObject.SetActive(false);
+    }
+
+    
+}
