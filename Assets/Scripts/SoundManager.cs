848923fae5d9f33736a8acfff73a35ff1cc98a90
rick-mingyu-liu <rickmyliu@gmail.com>
2024-10-25 17:02:07 +0800

    options page set

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -7,6 +7,10 @@ public class SoundManager : MonoBehaviour {
 
     [SerializeField] private AudioClipRefsSO audioClipRefsSO;
 
+    private float volume = 1f;
+
+
+
     private void Awake() {
         Instance= this;
     }
@@ -53,11 +57,22 @@ public class SoundManager : MonoBehaviour {
         PlaySound(audioClipArray[Random.Range(0, audioClipArray.Length)], position, volume);
     }
 
-    private void PlaySound(AudioClip audioClip, Vector3 position, float volume = 1f) {
-        AudioSource.PlayClipAtPoint(audioClip, position, volume);
+    private void PlaySound(AudioClip audioClip, Vector3 position, float volumeMultiplier = 1f) {
+        AudioSource.PlayClipAtPoint(audioClip, position, volumeMultiplier * volume);
     }
 
     public void PlayFootstepsSound(Vector3 position, float volume) {
         PlaySound(audioClipRefsSO.footstep, position, volume);
+    }
+
+    public void ChangeVolume() {
+        volume += .1f;
+        if (volume > 1f) {
+            volume = 0f;
+        }
+    }
+
+    public float GetVolume() {
+        return volume;
     }
 }
