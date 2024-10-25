a080e7f867e323cc77fdac4973909f4cc8fd4721
rick-mingyu-liu <rickmyliu@gmail.com>
2024-10-25 15:08:09 +0800

    loading theme implemented

--- Assets/Scripts/UI/MainMenuUI.cs
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -1,8 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class MainMenuUI : MonoBehaviour
 {
+    [SerializeField] private UnityEngine.UI.Button playButton;
+    [SerializeField] private UnityEngine.UI.Button quitButton;
+
+    private void Awake() {
+        playButton.onClick.AddListener(() => {
+            // click
+            Loader.Load(Loader.Scene.GameScene);
+        });
+
+        quitButton.onClick.AddListener(() => {
+            // click
+            Application.Quit();
+        });
+    }
+
     
+
 }
